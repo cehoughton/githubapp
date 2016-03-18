3b805a5a18fcf51cc0b7e4c676f021361b70e844
chris <cehoughton71@gmail.com>
2016-03-18 13:47:08 -0700

    slow

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -6,10 +6,10 @@ $(function() {
   $('#repo').click(function() {
     var ghrepo = $('#username').val();
     $('#username').val("");
-
-    // event.preventDefault();
+    $('.showRepo').text("Here is" + ghrepo  ".");
+    event.preventDefault();
     $.get('https://api.github.com/users/' + ghrepo + '?access_token=' + apiKey).then(function(response) {
-      console.log(response);
+        $('.showRepo').text("Here is" + ghrepo + "repos" + response.public_repos);
   });
 });
 });
