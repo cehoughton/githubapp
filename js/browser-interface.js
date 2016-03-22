09445d3e6a6307b842f89727e2c2fa09ec06027d
chris <cehoughton71@gmail.com>
2016-03-21 19:19:47 -0700

    add readme

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -3,13 +3,14 @@ var apiKey = require('./../.env').apiKey;
 
 
 $(function() {
-  $('#repo').click(function() {
+  $('#repo').submit(function() {
     var ghrepo = $('#username').val();
     $('#username').val("");
-    $('.showRepo').text("Here is" + ghrepo  ".");
+    $('.showRepo').text("Here is" + ghrepo);
     event.preventDefault();
     $.get('https://api.github.com/users/' + ghrepo + '?access_token=' + apiKey).then(function(response) {
-        $('.showRepo').text("Here is" + ghrepo + "repos" + response.public_repos);
+      console.log(response);
+        // $('.showRepo').text("Here is" + ghrepo + "repos" + response.public_repos);
   });
 });
 });
