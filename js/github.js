8950b7abdff82c86a5eaa811f9481edb7b3e3cf6
chris <cehoughton71@gmail.com>
2016-03-22 20:18:33 -0700

    non working loop

--- js/github.js
+++ js/github.js
@@ -3,9 +3,22 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(ghrepo) {
  $.get('https://api.github.com/users/' + ghrepo + '?access_token=' + apiKey).then(function(response) {
-   console.log(response);
-   $('.showRepo').text("Here is" + ghrepo + "repos" + response.public_repos);
+   var repos = response;
+   console.log(repos);
+
+  //  for(var i = 0; i < response.length; i++){
+  //     $(".repo").append("<li>" + response[i].name + "</li><br>");
+  //   }
+  //  for(var i = 0; i < response.length; i++)
+  //     $("#repoDescription").append("<li>" + response[i].description + "</li><br>"
+
+   $('.showRepo').text("Here is " + ghrepo + " repos" + response.public_repos);
  }).fail(function(error) {
+  //  for(var i = 0; i < response.length; i++)
+  //     $(".repo").append("<li>" + response[i].name + "</li><br>");
+  //  for(var i = 0; i < response.length; i++)
+  //     $("#repoDescription").append("<li>" + response[i].description + "</li><br>");
+
 
  });
 };
