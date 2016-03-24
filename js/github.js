3b69af21c1706c052048fc2a122e5aa318f8631b
chris <cehoughton71@gmail.com>
2016-03-23 17:56:46 -0700

    list of repos shows on page

--- js/github.js
+++ js/github.js
@@ -2,13 +2,13 @@ var apiKey = require('./../.env').apiKey;
 
 
 exports.getRepos = function(ghrepo) {
- $.get('https://api.github.com/users/' + ghrepo + '?access_token=' + apiKey).then(function(response) {
+ $.get('https://api.github.com/users/' + ghrepo + '/repos?access_token=' + apiKey).then(function(response) {
    var repos = response;
    console.log(repos);
 
-  //  for(var i = 0; i < response.length; i++){
-  //     $(".repo").append("<li>" + response[i].name + "</li><br>");
-  //   }
+   for(var i = 0; i < response.length; i++){
+      $("#repoName").append("<li>" + response[i].name + "</li><br>");
+    }
   //  for(var i = 0; i < response.length; i++)
   //     $("#repoDescription").append("<li>" + response[i].description + "</li><br>"
 
